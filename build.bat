ea76ce83aa09549e80b69cbfd6c0ee4b1498db3d
stakiran <stakiran@users.noreply.github.com>
2021-07-02 21:06:01 +0900

    fix .md.html to .html

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@ set targetdir=%selfdir%files
 
 pushd %targetdir%
 for %%f in (*.md) do (
-	pandoc -f markdown+emoji -t html4 %%f -o %%f.html
+	pandoc -f markdown+emoji -t html4 %%f -o %%~nf.html
 )
 popd
